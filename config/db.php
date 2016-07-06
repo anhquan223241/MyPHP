7a544df03234e418c11e15ddf0455920cdf0f3df
Minh Quan <anhquan223241@gmail.com>
2016-07-06 06:15:38 -0400

    add yii

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yiidb',
+    'username' => 'root',
+    'password' => 'Quans2anh',
+    'charset' => 'utf8',
+];
